313e8cb1d787d8de8d7ee824f1991c6458109414
Radoslaw <ko.rad@o2.pl>
2019-03-22 08:45:27 +0100

    RadEngine - added log submodule

--- RadEngine/src/RadEngine/EntryPoint.h
+++ RadEngine/src/RadEngine/EntryPoint.h
@@ -5,6 +5,10 @@
 
 extern RadEngine::Application* RadEngine::CreateApplication();
 
+// header files are not seen by the compiler. Instead, the C++ preprocessor replaces each #include statement
+// with the contents of the corresponding header file prior to sending the translation unit (.cpp file)
+// to the compiler m
+
 int main(int argc, char** argv)
 {
 	printf("It works!");
